04b16d5c3e9fa6b28706d39ae4b5b19b29adb476
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-12-12 03:11:13 +0100

    refactor: internalize get/set

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,64 @@
-import { assocPath, dissocPath, path } from 'rambda'
+import { dissocPath } from 'rambda'
 
+type Indexable<T = any> = Record<string | number, T>
 type Path<O, K extends keyof O = keyof O> = K extends string | number
   ? O[K] extends infer V
     ? `${K}`| (V extends Record<string, unknown> ? `${K}.${Path<V>}` : never)
     : never
   : never
 
+function get(obj: object, path: string[]) {
+  if (obj == null)
+    return undefined
+
+  let value = obj
+
+  for (let i = 0; i < path.length; i++) {
+    if (value == null || (value as Indexable)[path[i]] == null)
+      return undefined
+
+    value = (value as Indexable)[path[i]]
+  }
+
+  return value
+}
+
+function set(path: string[], value: any, obj: object) {
+  if (path.length === 0)
+    return value
+
+  const idx = path[0]
+  if (path.length > 1) {
+    value = set(
+      Array.prototype.slice.call(path, 1),
+      value,
+      typeof obj !== 'object'
+        || obj === null
+        || !Object.prototype.hasOwnProperty.call(obj, idx)
+        ? Number.isInteger(Number(path[1]))
+          ? []
+          : {}
+        : (obj as Indexable)[idx],
+    )
+  }
+
+  if (Number.isInteger(Number(idx)) && Array.isArray(obj))
+    return (obj.slice() as Indexable)[idx]
+
+  return Object.assign({}, obj, { [idx]: value })
+}
+
 export function deepPickUnsafe(obj: object, paths: Array<string>) {
   return paths
-    .map<[string, unknown]>(p => [p, path(p, obj)])
+    .map(p => p.split('.'))
+    .map<[string[], unknown]>(p => [p, get(obj, p)])
     .filter(t => !!t[1])
-    .reduce((acc, cur) => assocPath(cur[0], cur[1], acc), {})
+    .reduce((acc, cur) => set(cur[0], cur[1], acc), {})
 }
 
 export function deepPick<O extends object>(obj: O, paths: Array<Path<O>>) {
   return deepPickUnsafe(obj, paths)
 }
-
 export function deepOmitUnsafe(obj: object, paths: Array<string>) {
   return paths
     .reduce((acc, cur) => dissocPath(cur, acc), obj)
