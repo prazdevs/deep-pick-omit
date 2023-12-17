4f5ccfd401247889ec3ae7a43b0a31978a9ca39f
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-12-17 17:39:25 +0100

    test: add typechecking tests

---
+++ test/index.test-d.ts
@@ -0,0 +1,40 @@
+import { expectTypeOf, test } from 'vitest'
+import {
+  type Path,
+  deepOmit,
+  deepOmitUnsafe,
+  deepPick,
+  deepPickUnsafe,
+} from '../src/index'
+
+const o = {
+  a: {
+    b: 'this',
+    c: 'not this',
+  },
+  d: 'this',
+}
+const fails = ['f']
+const passes = ['a', 'a.b', 'd'] satisfies Path<typeof o>[]
+
+test('deepPick', () => {
+  // @ts-expect-error `e` is not a key of `obj`
+  expectTypeOf(deepPick<typeof o>).toBeCallableWith(o, fails)
+  expectTypeOf(deepPick<typeof o>).toBeCallableWith(o, passes)
+})
+
+test('deepOmit', () => {
+  // @ts-expect-error `e` is not a key of `obj`
+  expectTypeOf(deepOmit<typeof o>).toBeCallableWith(o, fails)
+  expectTypeOf(deepOmit<typeof o>).toBeCallableWith(o, passes)
+})
+
+test('deepPickUnsafe', () => {
+  expectTypeOf(deepPickUnsafe).toBeCallableWith(o, fails)
+  expectTypeOf(deepPickUnsafe).toBeCallableWith(o, passes)
+})
+
+test('deepOmitUnsafe', () => {
+  expectTypeOf(deepOmitUnsafe).toBeCallableWith(o, fails)
+  expectTypeOf(deepOmitUnsafe).toBeCallableWith(o, passes)
+})
