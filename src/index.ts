7d30e930f98b33edb5e53699b6a1bdb0779b85ed
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-12-17 11:37:55 +0100

    feat: internalize utils and drop external dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,29 @@
-type Indexable<T = any> = Record<string | number, T>
-type Path<O, K extends keyof O = keyof O> = K extends string | number
+import { get, set, unset } from './utils'
+
+export type Path<O, K extends keyof O = keyof O> = K extends string | number
   ? O[K] extends infer V
     ? `${K}`| (V extends Record<string, unknown> ? `${K}.${Path<V>}` : never)
     : never
   : never
-
-function get(obj: object, path: string[]) {
-  if (obj == null)
-    return undefined
-
-  let value = obj
-
-  for (let i = 0; i < path.length; i++) {
-    if (value == null || (value as Indexable)[path[i]] == null)
-      return undefined
-
-    value = (value as Indexable)[path[i]]
-  }
-
-  return value
-}
-
-function set(path: string[], value: any, obj: object) {
-  if (path.length === 0)
-    return value
-
-  const idx = path[0]
-  if (path.length > 1) {
-    value = set(
-      Array.prototype.slice.call(path, 1),
-      value,
-      typeof obj !== 'object'
-        || obj === null
-        || !Object.prototype.hasOwnProperty.call(obj, idx)
-        ? Number.isInteger(Number(path[1]))
-          ? []
-          : {}
-        : (obj as Indexable)[idx],
-    )
-  }
-
-  if (Number.isInteger(Number(idx)) && Array.isArray(obj))
-    return (obj.slice() as Indexable)[idx]
-
-  return Object.assign({}, obj, { [idx]: value })
-}
 
 export function deepPickUnsafe(obj: object, paths: Array<string>) {
   return paths
     .map(p => p.split('.'))
     .map<[string[], unknown]>(p => [p, get(obj, p)])
     .filter(t => !!t[1])
-    .reduce((acc, cur) => set(cur[0], cur[1], acc), {})
+    .reduce((acc, cur) => set(acc, cur[1], cur[0]), {})
 }
 
 export function deepPick<O extends object>(obj: O, paths: Array<Path<O>>) {
   return deepPickUnsafe(obj, paths)
 }
 
-// export function deepOmitUnsafe(obj: object, paths: Array<string>) {
-//   return paths
-//     .reduce((acc, cur) => dissocPath(cur, acc), obj)
-// }
+export function deepOmitUnsafe(obj: object, paths: Array<string>) {
+  return paths
+    .map(p => p.split('.'))
+    .reduce((acc, cur) => unset(acc, cur), obj)
+}
 
-// export function deepOmit<O extends object>(obj: O, paths: Array<Path<O>>) {
-//   return deepOmitUnsafe(obj, paths)
-// }
+export function deepOmit<O extends object>(obj: O, paths: Array<Path<O>>) {
+  return deepOmitUnsafe(obj, paths)
+}
---
+++ src/utils.ts
@@ -0,0 +1,82 @@
+type Indexable<T = any> = Record<string | number, T>
+
+export function get(obj: object, path: Array<string | number>) {
+  if (obj == null)
+    return undefined
+
+  let value = obj
+
+  for (let i = 0; i < path.length; i++) {
+    if (value == null || (value as Indexable)[path[i]] == null)
+      return undefined
+
+    value = (value as Indexable)[path[i]]
+  }
+
+  return value
+}
+
+export function set(obj: object, value: any, path: Array<string | number>) {
+  if (path.length === 0)
+    return value
+
+  const idx = path[0]
+  if (path.length > 1) {
+    value = set(
+      typeof obj !== 'object'
+        || obj === null
+        || !Object.prototype.hasOwnProperty.call(obj, idx)
+        ? Number.isInteger(Number(path[1]))
+          ? []
+          : {}
+        : (obj as Indexable)[idx],
+      value,
+      Array.prototype.slice.call(path, 1),
+    )
+  }
+
+  if (Number.isInteger(Number(idx)) && Array.isArray(obj))
+    return (obj.slice() as Indexable)[idx]
+
+  return Object.assign({}, obj, { [idx]: value })
+}
+
+export function unset(obj: object, path: Array<string | number>): object {
+  if (obj == null || path.length === 0)
+    return obj
+
+  if (path.length === 1) {
+    if (obj == null)
+      return obj
+
+    if (Number.isInteger(path[0]) && Array.isArray(obj))
+      return Array.prototype.slice.call(obj, 0).splice(path[0] as number, 1)
+
+    const result: Indexable = {}
+    for (const p in obj)
+      result[p] = (obj as Indexable)[p]
+
+    delete result[path[0]]
+    return result
+  }
+
+  if ((obj as Indexable)[path[0]] == null) {
+    if (Number.isInteger(path[0]) && Array.isArray(obj))
+      return Array.prototype.concat.call([], obj)
+
+    const result: Indexable = {}
+    for (const p in obj)
+      result[p] = (obj as Indexable)[p]
+
+    return result
+  }
+
+  return set(
+    obj,
+    unset(
+      (obj as Indexable)[path[0]],
+      Array.prototype.slice.call(path, 1),
+    ),
+    [path[0]],
+  )
+}
